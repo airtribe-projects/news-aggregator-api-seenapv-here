54b76758223cad92a0d5d728b84403ae4f0cc86f
“seenapv-here <seenapv@gmail.com>
2025-01-11 22:09:09 +0530

    Version 1

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/users');
+const preferencesRoutes = require('./routes/preferences');
+const newsRoutes = require('./routes/news');
 
+require('dotenv').config(); // Load environment variables
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('Something bad happened', err);
-    }
-    console.log(`Server is listening on ${port}`);
-});
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log('Connected to the MongoDB');
+    app.listen(port, (err) => {
+        if (err) {
+            return console.log('Something bad happened', err);
+        }
+        console.log(`Server is listening on ${port}`);
+    });
+})
+.catch((err) => console.error('Could not connect to MongoDB:', err));
 
-
+// Routes
+app.use('/users', userRoutes);
+app.use('/users', preferencesRoutes);
+app.use('/', newsRoutes);
 
 module.exports = app;
