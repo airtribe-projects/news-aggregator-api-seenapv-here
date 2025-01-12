cbff0454a121b5cf6fe58fd36e4d82bf66ea16af
“seenapv-here <seenapv@gmail.com>
2025-01-12 18:51:36 +0530

    Updated with cache

--- routes/news.js
+++ routes/news.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const authenticateToken = require('../middlewares/authMiddleware');
 const User = require('../models/user');
+const cache = require("../utils/cache");
 
 const router = express.Router();
 
@@ -27,20 +28,31 @@ router.get('/news', authenticateToken, async (req, res) => {
             return res.status(400).json({ message: 'No preferences set. Please update your preferences.' });
         }
 
-        // Build API query parameters
-        const apiUrl = `${process.env.NEWS_API_BASE_URL}`;
-        const query = preferences.join(' OR '); // Combine preferences with "OR" for the query
-        const params = {
-            apiKey: process.env.NEWS_API_KEY,
-            language: 'en', // Default to English; you can customize based on requirements
-            q: query,
-        };
-        
-        // Fetch news articles
-        const response = await axios.get(apiUrl, { params });
-        
-        // Send response to client
-        res.status(200).json({ articles: response.data.articles });
+        const cacheKey = `news_${req.user.id}`;
+        let news = cache.get(cacheKey);
+        if (news) {
+            console.log('Fetching news from cache');
+            return res.status(200).json({ articles: news });
+        }
+        else{
+            console.log('Fetching news from API');
+       
+            // Build API query parameters
+            const apiUrl = `${process.env.NEWS_API_BASE_URL}`;
+            const query = preferences.join(' OR '); // Combine preferences with "OR" for the query
+            const params = {
+                apiKey: process.env.NEWS_API_KEY,
+                language: 'en', // Default to English; you can customize based on requirements
+                q: query,
+            };
+            
+            // Fetch news articles
+            const response = await axios.get(apiUrl, { params });
+            
+            // Send response to client
+            res.status(200).json({ articles: response.data.articles });
+            cache.set(cacheKey, response.data.articles);
+        }
     } catch (error) {
         console.error('Error fetching news:', error);
         res.status(500).json({ message: 'Failed to fetch news articles.', error: error.message });
